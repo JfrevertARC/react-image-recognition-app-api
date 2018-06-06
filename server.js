6ee4baaf4b84f5872a29fef690a1d0315bd7dd5d
Justin Frevert <jgaingringer@gmail.com>
2018-06-05 22:34:23 -0400

    changed db config

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var knex = require('knex')
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-asymmetrical-89824',
       user : 'frevertj',
       password : '',
       database : 'face-recog'
